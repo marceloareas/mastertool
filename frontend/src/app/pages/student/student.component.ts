6cad83ed18b9d62053989dfcd960c4bd547d3006
LaNicolau <larissanicolau74@gmail.com>
2024-04-03 20:08:20 -0300

    campo de pesquisa e quantidade de itens na tabela na tela de aluno

--- frontend/src/app/pages/student/student.component.ts
+++ frontend/src/app/pages/student/student.component.ts
@@ -1,37 +1,56 @@
-import { Component, inject } from '@angular/core';
+import { Component, ViewChild, inject } from '@angular/core';
 import { TopBarComponent } from '../../components/top-bar/top-bar.component';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { ModalStudentComponent } from './components/modal-student/modal-student.component';
-import { MatTableModule } from '@angular/material/table';
-import { FormControl, FormGroup } from '@angular/forms';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { ClassService } from '../../services/class/class.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatLabel } from '@angular/material/form-field';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'app-student',
   standalone: true,
-  imports: [TopBarComponent, MatDialogModule, MatTableModule],
+  imports: [
+    TopBarComponent,
+    MatDialogModule,
+    MatTableModule,
+    MatLabel,
+    MatFormFieldModule,
+    MatInput,
+    MatPaginatorModule
+  ],
   templateUrl: './student.component.html',
   styleUrl: './student.component.scss',
 })
 export class StudentComponent {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
   private dialog = inject(MatDialog);
   private classService = inject(ClassService);
-  showUpload: boolean = false;
-  displayedColumns: string[] = ['nome'];
-  dataSource: any;
 
-     ngOnInit(){
-    this.classService.get().subscribe( data => {
-      this.dataSource = data;
-    })
+  displayedColumns: string[] = ['nome'];
+  dataSource!: MatTableDataSource<MatPaginator>;
+
+  constructor() {
+    this.classService.get().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+    });
   }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
   openModal() {
     this.dialog
       .open(ModalStudentComponent, {
         width: '600px',
       })
-      .afterClosed().subscribe(result => {
-        console.log(`Dialog result: ${result}`);
-      });
+  }
+
+  filter(event: Event){
+    this.dataSource.filter = (event.target as HTMLInputElement)?.value;
   }
 }
