d81123b22f5539a1f87f01555aee23d1a5606429
AnaClaraCS <anaclaracostasouza@yahoo.com.br>
2024-11-27 00:12:43 -0300

    Modificando o service de projeto

--- frontend/src/app/services/project/project.service.ts
+++ frontend/src/app/services/project/project.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProjectService {
+  private http = inject(HttpClient);
 
-  constructor() { }
+  post = (dados: any) => {
+    return this.http.post<any>(
+      'http://127.0.0.1:8000/projetos/cadastrar/',
+      dados
+    );
+  };
+
+  get = (id: string = '') => {
+    return this.http.get<any>('http://127.0.0.1:8000/projetos/' + id);
+  };
+
+  delete = (id: string) => {
+    return this.http.delete<any>('http://127.0.0.1:8000/projetos/excluir/' + id);
+  };
+
+  put = (id: string, dados: any) => {
+    return this.http.put<any>(
+      'http://127.0.0.1:8000/projetos/editar/' + id,
+      dados
+    );
+  };
+
 }
