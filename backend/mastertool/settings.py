87ce88f4571c02fe99b14be0d8e3ff2205e59a69
RodrigoKasama <rodrigo.parracho@gmail.com>
2025-05-26 18:28:05 -0300

    bug: Django não reconhece outro host senão o localhost, o que impede de se conectar com o outro host/container (db)

--- backend/mastertool/settings.py
+++ backend/mastertool/settings.py
@@ -93,7 +93,9 @@ DATABASES = {
         'NAME': os.environ.get('DATABASE_NAME'),
         'USER': os.environ.get('DATABASE_USER'),
         'PASSWORD' : os.environ.get('DATABASE_PASSWORD'),
-        'HOST' : 'localhost'
+        'HOST' : 'db',
+        'PORT' : '5432',
+        
     }
 }
 
